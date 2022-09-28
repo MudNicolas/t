626effa22d97f2ea2f9a71d01d5ce3d8e0120e12
Nicolas <1075039737@qq.com>
2022-09-28 20:53:37 +0800

    初始化题库

--- 种花/main.py
+++ 种花/main.py
@@ -1,3 +1,23 @@
-a = map(int, input(), split())
+a = list(map(int, input().split()))
 m = int(input())
+
+
+def fp(x, y):
+    t = 0
+    for i in range(x, y+1):
+        t += a[i]
+    print(t)
+
+
+def cp(x, y, k):
+    global a
+    for i in range(x, y+1):
+        a[i] += k
+
+
 for i in range(m):
+    s = input().split()
+    if s[0] == 'query':
+        fp(int(s[1]), int(s[2]))
+    else:
+        cp(int(s[1]), int(s[2]), int(s[3]))
---
+++ 萌萌填数字/main.py
@@ -0,0 +1,7 @@
+n = int(input())
+cnt, now, ans = 1, 1, 0
+while now <= n:
+    ans += 1
+    now += cnt
+    cnt += 1
+print(ans)
